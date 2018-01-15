9c2a20b5463e22d2691c7f83ddeb8075285d0e6a
orcunsavas <35468902+orcunsavas@users.noreply.github.com>
2018-01-15 23:55:08 +0300

    Add files via upload

---
+++ Bilmece.py
@@ -0,0 +1,14 @@
+while True:
+	print("Lütfen ekrandaki bilmecenin cevabını veriniz")
+	user_input = input(": ")
+	if user_input == "Yarrak!" :
+		print("Eaveeeğtt! YaraaaĞĞkk! Yarrağı bildiniz! Yarrağı bildiniz! YarraaĞk! Evet bu işte!")
+	elif user_input == "yarrak!":
+		print("Eaveeeğtt! YaraaaĞĞkk! Yarrağı bildiniz! Yarrağı bildiniz! YarraaĞk! Evet bu işte!")
+	elif user_input == "Yarrak":
+		print("Eaveeeğtt! YaraaaĞĞkk! Yarrağı bildiniz! Yarrağı bildiniz! YarraaĞk! Evet bu işte!")
+	elif user_input == "yarrak":
+		print("Eaveeeğtt! YaraaaĞĞkk! Yarrağı bildiniz! Yarrağı bildiniz! YarraaĞk! Evet bu işte!")
+	else:
+    		print("Ya bi boku bilemediniz ya!")
+	break
---
+++ Yerli hesap makinesi.py
@@ -0,0 +1,39 @@
+while True:
+	print("Seçenekler:")
+	print("İki sayıyı birbirine eklemek için 'ekle' yazın")
+	print("İki sayıyı birbirinden çıkartmak için 'çıkart' yazın")
+	print("İki sayıyı birbiriyle çarpmak için 'çarp' yazın")
+	print("İki sayıyı birbirine bölmek için 'böl' yazın")
+	print("İki sayının birinin diğer üssünü almak için 'üssü' yazın")	
+	print("Çıkmak için 'çık' yazın")
+	user_input = input(": ")
+
+	if user_input == "çık":
+		break
+	elif user_input == "ekle":
+		num1=float(input("Bir sayı giriniz: "))
+		num2=float(input("Diğer sayıyı giriniz: "))
+		result=str(num1+num2)
+		print("Cevap "+result)
+	elif user_input == "çıkart":
+		num1=float(input("Bir sayı giriniz: "))
+		num2=float(input("Diğer sayıyı giriniz: "))
+		result=str(num1-num2)
+		print("Cevap "+result)
+	elif user_input == "çarp":
+		num1=float(input("Bir sayı giriniz: "))
+		num2=float(input("Diğer sayıyı giriniz: "))
+		result=str(num1*num2)
+		print("Cevap "+result)
+	elif user_input == "böl":
+		num1=float(input("Bir sayı giriniz: "))
+		num2=float(input("Diğer sayıyı giriniz: "))
+		result=str(num1/num2)
+	elif user_input == "üssü":
+		num1=float(input("Üssü alınacak sayıyı giriniz: "))
+		num2=float(input("Kaç üssü?: "))
+		result=str(num1**num2)
+		print("Cevap "+result)	
+	else:
+		print("Yanlış giriş yaptınız, lütfen tekrar deneyin.")
+	
