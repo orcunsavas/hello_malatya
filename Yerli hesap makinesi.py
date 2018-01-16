ad9f58b85e72591aecde2f2ef491f4155d97502f
orcunsavas <35468902+orcunsavas@users.noreply.github.com>
2018-01-16 10:13:09 +0300

    Add files via upload

--- Yerli hesap makinesi.py
+++ Yerli hesap makinesi.py
@@ -1,16 +1,26 @@
 while True:
 	print("Seçenekler:")
-	print("İki sayıyı birbirine eklemek için 'ekle' yazın")
+	print("İki sayıyı birbirine eklemek için 'topla' yazın")
 	print("İki sayıyı birbirinden çıkartmak için 'çıkart' yazın")
 	print("İki sayıyı birbiriyle çarpmak için 'çarp' yazın")
 	print("İki sayıyı birbirine bölmek için 'böl' yazın")
 	print("İki sayının birinin diğer üssünü almak için 'üssü' yazın")	
-	print("Çıkmak için 'çık' yazın")
+	print("Çıkmak için 'çık' veya 'exit' yazın")
 	user_input = input(": ")
 
 	if user_input == "çık":
 		break
-	elif user_input == "ekle":
+	elif user_input == "exit":
+		break
+	elif user_input == "Hello":
+		print("Hello Malatya, ne var ne yok orda?")
+	elif user_input == "hello":
+		print("Hello Malatya, ne var ne yok orda?")
+	elif user_input == "Hello Malatya":
+		print("Hello Malatya, ne var ne yok orda?")
+	elif user_input == "Hello malatya":
+		print("Hello Malatya, ne var ne yok orda?")
+	elif user_input == "topla":
 		num1=float(input("Bir sayı giriniz: "))
 		num2=float(input("Diğer sayıyı giriniz: "))
 		result=str(num1+num2)
